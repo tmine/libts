31cddc03eeebdaad528dea73a980deb004b95a0b
Adrian Bertschi <adrian.bertschi@clip.ch>
2014-02-06 11:06:29 +0100

    - reverted ResourceLoader to before XDomainRequest. (It's not working)

--- tsc/ui/ResourceLoader.ts
+++ tsc/ui/ResourceLoader.ts
@@ -2,7 +2,7 @@ module tsc.ui{
 
 	// XMLHttpRequest for IE6, IE5
 	if (!XMLHttpRequest) {
-	   XMLHttpRequest = ActiveXObject("Microsoft.XMLHTTP");
+	      XMLHttpRequest = ActiveXObject("Microsoft.XMLHTTP");
 	}
 
 	export class ResourceLoader{    
@@ -15,43 +15,19 @@ module tsc.ui{
 
 		private _load(xml : boolean, path : string, callback? : Function) : any {
 			var xhr = new XMLHttpRequest();
-            if(xml && typeof XDomainRequest != "undefined"){
-               console.log("XDomainRequest is defined");
-               xhr = <any>new XDomainRequest;
-            }
 			xhr.onreadystatechange = function(){
 				if (xhr.readyState == 4/* && xhr.status == 200*/){
 					if(callback){
-						if(xml){
-                            if(typeof xhr.responseXML != undefined){
-                                callback(xhr.responseXML);
-                            } else {
-                                var xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
-                                xmlDoc.async=false;
-                                xmlDoc.loadXML(xhr.responseText);
-                                callback(xmlDoc);
-                            }
-						} else {
-                            callback(xhr.responseText);
-                        }
+						if(xml) callback(xhr.responseXML);
+						else 	callback(xhr.responseText);
 					}
 				}
 			};
 			xhr.open("GET", path, (callback != undefined));
 			xhr.send();
 
-			if(xml){
-                if(typeof xhr.responseXML != undefined){
-                    return xhr.responseXML;
-                } else {
-                    var xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
-                    xmlDoc.async=false;
-                    xmlDoc.loadXML(xhr.responseText);
-                    return xmlDoc;
-                }
-            } else {
-                return xhr.responseText;
-            }
+			if(xml) return xhr.responseXML;
+			else 	return xhr.responseText;
 		}
 	}
 }
